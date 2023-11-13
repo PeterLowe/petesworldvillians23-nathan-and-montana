ce1ec9449c6fbb49f207b951597db52f63d4b6eb
(Student  C00296838) Nathan Mas <c00296838@setu.ie>
2023-11-13 18:10:27 +0000

    Added Ys

--- MontanaNathan/MontanaNathan/source.cpp
+++ MontanaNathan/MontanaNathan/source.cpp
@@ -1,1 +1,60 @@
+// Program used to find countries
+// 13/11/2023
+// C00296838
+// Nathan Mas
+// Estimated time 120 minutes
+// Actual time 120 minutes
+
+#include <iostream>
+
+int main()
+{
+	int region = 0; // user choosen region number
+	char letter = ' '; // user choosen letter
+	std::cout << "Pete's World select a region\n 1. Americas\n 2. Europe\n 3. Africa\n 4. Asia\n 5. Rest of the World" << std::endl;
+	std::cin >> region;
+	if (region < 1 || region > 5)
+	{
+		std::cout << "You need to select a number between 1 and 5" << std::endl;
+		return 0;
+	}
+	std::cout << "Pick a letter from A to A or Z to Z" << std::endl; // change this line each iteration
+	std::cin >> letter;
+	if (letter < 'A' || letter > 'Z') // not a capital letter
+	{
+		if (letter < 'a' || letter > 'z') // not a lower case letter
+		{
+			std::cout << "You need to select a letter from A to Z" << std::endl;
+			return 0;
+		}
+	}
+	if (letter == 'y' || letter == 'Y') // written by Pete
+	{
+		if (region == 1)
+		{
+			std::cout << "There are no Ys in the Americas" << std::endl; // default negative response
+		}
+		if (region == 2)
+		{
+			std::cout << "There are no Ys in Europe" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "There are no Ys in Africa" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no Ys in Asia" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Yemen Arrabian Peninsula" << std::endl;
+		}
+
+	}
+
+
+
+	return 1;
+}
 
